76113a6028b29c1c5ec1f26760b2b8096e611158
ANUJ ARORA <anuj@ANUJs-MacBook-Pro.local>
2012-09-18 22:28:48 -0400

    Added displayUrl and Url.

--- relevance/query/QueryRecord.java
+++ relevance/query/QueryRecord.java
@@ -4,12 +4,14 @@ public class QueryRecord {
 	
 	private String title;
 	private String url;
+	private String displayUrl;
 	private String description;
 	
-	public QueryRecord(String title, String url, String description)
+	public QueryRecord(String title, String url, String displayUrl, String description)
 	{
 		this.title= title;
 		this.url = url;
+		this.displayUrl = displayUrl;
 		this.description = description;
 	}
 	public String getTitle() {
@@ -18,6 +20,10 @@ public class QueryRecord {
 	
 	public String getUrl() {
 		return url;
+	}
+	
+	public String displayUrl() {
+		return displayUrl;
 	}
 	
 	public String getDescription() {
