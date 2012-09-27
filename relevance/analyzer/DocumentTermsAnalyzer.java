2a5cdf84d84201aad3fdd7f5daff3f1787f95573
Anuj <e290305@e290305-OptiPlex-780.(none)>
2012-09-26 20:41:16 -0400

    Added an analyzer for Document terms compator

---
+++ relevance/analyzer/DocumentTermsAnalyzer.java
@@ -0,0 +1,111 @@
+package analyzer;
+
+import indexer.DocumentIndexer;
+import indexer.TermFreq;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Vector;
+
+import query.QueryRecord;
+import util.Global;
+
+public class DocumentTermsAnalyzer extends TermAnalyzer {
+
+	private Vector<Vector<TermFreq> > tf;
+	
+	public DocumentTermsAnalyzer(String[] query) {
+		super(query);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Map<String, Double> rateTerms(Vector<QueryRecord> results,
+			String[] query) {
+		
+		Map<String, Double> overallRates = new HashMap<String, Double>();
+		
+		DocumentIndexer indexer;
+		Vector<Vector<String> > allDocWords = new Vector<Vector<String> >();
+		
+		
+		try {
+			indexer = new DocumentIndexer();
+			
+			for (QueryRecord result : Global.getPositives()) 
+			{
+				Vector<String> list = result.getHtmlPageWords();	
+				allDocWords.add(list);
+			}
+			indexer.addDocuments(allDocWords);
+			
+			tf = indexer.getTermFrequencies();	
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		overallRates = analyzeTermFreq();
+		
+		//print the maxscore
+		Double maxScore = 0.0;
+		String expandTerm = null;
+		for (Map.Entry<String, Double> tr : overallRates.entrySet()) {			
+			if (tr.getValue() > maxScore) {
+				maxScore = tr.getValue();
+				expandTerm = tr.getKey();
+			}
+			
+		}
+		
+		System.out.printf("document term analyzer: %s - %s%n", expandTerm, maxScore);
+		
+		return overallRates;
+	}
+	
+	private Map<String, Double> analyzeTermFreq() {
+		
+		Vector<HashMap<String, Double>> mapVec = new Vector<HashMap<String, Double>>();
+		for (Vector<TermFreq> theDocument : tf )
+		{
+			HashMap<String, Double> theMap = new HashMap<String, Double>();
+	
+			//calculate total terms
+			int totalTerms = 0;
+			for (TermFreq theTerm : theDocument)
+				totalTerms = totalTerms + theTerm.getFreq();
+			
+			for (TermFreq theTerm : theDocument)
+			{
+				String term = theTerm.getTerm();
+				if ( Global.getCurrentQuery().contains(term.trim().toLowerCase()) )
+					continue; 
+				else
+					theMap.put(theTerm.getTerm(), (1.0 * theTerm.getFreq() / totalTerms));				
+				
+				//insert the map in the vector
+				mapVec.add(theMap);
+			}
+		}
+				
+		Map<String, Double> retval= new HashMap<String, Double>();
+			
+		for (HashMap<String, Double> termMap : mapVec ) {
+			for (Map.Entry<String, Double> entry : termMap.entrySet()) {
+				if (!retval.containsKey(entry.getKey())) {
+					retval.put(entry.getKey(), entry.getValue());
+				}
+				else {
+					retval.put(entry.getKey(), 
+							retval.get(entry.getKey()) + entry.getValue());
+				}
+			}
+		}
+		
+		return retval;
+	}
+
+}
